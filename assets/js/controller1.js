fd73be00738979cf10f3dd89f312d985d3192a04
chao.zhang <chao.zhang@dianping.com>
2015-07-15 14:48:42 +0800

    详情页展示

--- assets/js/controller1.js
+++ assets/js/controller1.js
@@ -1,6 +1,27 @@
 /**
  * Created by zcfrank1st on 7/14/15.
  */
-app1.controller('mdsController1', function ($scope) {
+app1.controller('mdsController1', function ($scope, $location, $resource) {
+    var saveDescription = $resource('/mds/change/desc');
+    var saveColumnInfo = $resource('/mds/change/desc/column');
 
+    var currentUrl = $location.absUrl();
+    var id = currentUrl.substr(currentUrl.length - 2, 1);
+
+    $scope.description = true;
+    $scope.saveContent = function () {
+        var data = {id: id, description: $scope.descriptionInfo};
+        saveDescription.save(data, function (r) {
+            if (r.info === "yes") {
+                location.href=$location.absUrl();
+            } else if(r.info === "no") {
+                $scope.descAlert = true;
+
+            } else {
+                $scope.descAlert = true;
+            }
+        });
+    };
+
+    
 });
--- mds/views.py
+++ mds/views.py
@@ -12,7 +12,17 @@ def index(request):
 # restful 方法
 # POST
 def change_table_description(request):
-    pass
+    if request.method == 'POST':
+        req = json.loads(request.body)
+        try:
+            ItemTable.objects.filter(table_id=req['id']).update(description=req['description'])
+        except :
+            return HttpResponse(json.dumps({"info": "no"}))
+        return HttpResponse(json.dumps({"info": "yes"}))
+    else:
+        return HttpResponse(json.dumps({"info": "no"}))
+
+
 # POST
 def change_table_column_description(request):
     pass
@@ -30,5 +40,5 @@ def get_all_tables(request):
 # GET
 def get_peculiar_table_info(request, id):
     result = ItemTable.objects.get(table_id=id)
-    values = {"name": result.table_name, "description": result.description}
+    values = {"name": result.table_name, "description": result.description, "create_table": result.create_table_info, "blood": result.blood_relation}
     return render(request, 'detail.html', values)
